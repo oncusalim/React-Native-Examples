b282528bf26a185a9fcbae6d1d72ffb868bd20f4
oncusalim <salimoncu.fb@gmail.com>
2021-01-25 00:40:29 +0300

    custom component example

--- App.js
+++ App.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
 
+} from 'react-native';
+import MyComponent from './src/components/MyComponent';
 
+function myFunction() {
+  const myArray = ['İstanbul', 'İzmir', 'Bursa', 'Ankara'];
 
+  return myArray.map((city) => {
+    return <Text>{city}</Text>;
+
+  })
+}
+
+const my_name = "Salim";
+const isAdmin = true;
 const App = () => {
-    return (
-        <SafeAreaView>
-            <View style={styles.container}>
-                <Text style={styles.text}>HELLO!!</Text>
-            </View>
-        </SafeAreaView >
-    )
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>MERHABA!!</Text>
+      </View>
+      <MyComponent />
+      <View style={styles.container}>
+        {isAdmin ? <Text style={{ fontSize: 30 }}>Admin Kullanıcısı</Text> : null}
+        {isAdmin && <Text>AdminKullanıcısı</Text>}
+        {true && <Text></Text>}
+        {myFunction()}
+      </View>
+    </SafeAreaView>
+  );
 };
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'purple',
-        padding: 10,
-        margin: 10,
-        borderRadius: 15,
-    },
-    text: {
-        color: "white",
-        fontSize: 30,
-        textAlign: 'center',
-        margin: 15
-    }
-});
+
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'powderblue',
+    padding: 10,
+    margin: 10,
+    borderRadius: 15,
+  },
+  text: {
+    color: "red",
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  name: {
+    margin: 10,
+    fontSize: 20,
+  }
+});
