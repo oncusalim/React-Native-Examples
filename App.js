0ad53a978ab36f9d6901c097ff86a195acc2e37a
oncusalim <salimoncu.fb@gmail.com>
2021-01-28 11:46:03 +0300

    Flex Example

--- App.js
+++ App.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   View,
   Text,
+  StyleSheet,
 
 } from 'react-native';
-import MyComponent from './src/components/MyComponent';
 
-function myFunction() {
-  const myArray = ['İstanbul', 'İzmir', 'Bursa', 'Ankara'];
-
-  return myArray.map((city) => {
-    return <Text>{city}</Text>;
-
-  })
-}
-
-const my_name = "Salim";
-const isAdmin = true;
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
       <View style={styles.container}>
-        <Text style={styles.text}>MERHABA!!</Text>
-      </View>
-      <MyComponent />
-      <View style={styles.container}>
-        {isAdmin ? <Text style={{ fontSize: 30 }}>Admin Kullanıcısı</Text> : null}
-        {isAdmin && <Text>AdminKullanıcısı</Text>}
-        {true && <Text></Text>}
-        {myFunction()}
+        <View style={styles.top}>
+          <View style={styles.topInside}>
+            <View style={styles.circle}>
+              <Text style={{ color: 'black', alignItems: 'center' }}>Orange Circle</Text>
+            </View>
+            <View style={styles.label}>
+              <Text>Yellow Box</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.bottom}>
+          <View style={styles.bottombig}>
+            <Text style={{ color: 'white' }}>Black Box</Text>
+          </View>
+          <View style={styles.bottomsmall}>
+            <Text style={styles.textStyle}>Brown Box</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
-  );
+  )
 };
 
 
@@ -42,17 +40,67 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'powderblue',
-    padding: 10,
-    margin: 10,
-    borderRadius: 15,
+    flex: 1,
   },
-  text: {
-    color: "red",
-    fontSize: 30,
-    textAlign: 'center',
+  top: {
+    backgroundColor: 'red',
+    flex: 1,
+
   },
-  name: {
-    margin: 10,
-    fontSize: 20,
+  topInside: {
+    flexDirection: 'row',
+    backgroundColor: 'gray',
+    margin: 20,
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottom: {
+    flexDirection: 'row',
+    backgroundColor: 'pink',
+    flex: 2,
+    justifyContent: 'center',
+    padding: 20
+  },
+  circle: {
+    backgroundColor: 'orange',
+    margin: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    width: 100,
+    height: 100
+
+
+
+  },
+  label: {
+    backgroundColor: 'yellow',
+    borderRadius: 20,
+    padding: 20,
+    margin: 20
+  },
+  bottombig: {
+    backgroundColor: 'black',
+    borderRadius: 20,
+    margin: 30,
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottomsmall: {
+    backgroundColor: 'brown',
+    borderRadius: 40,
+    flex: 1,
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textStyle: {
+    transform: [{ rotate: '-90deg' }],
+    color: 'white'
   }
-});
+
+
+})
